5d13e3c151ca3af57d79fcf1d50feb1398bd5bd8
alaidine <aa.alimlinde@gmail.com>
2022-12-23 21:25:30 +0400

    added the login and registration pages

---
+++ src/routes/Login/+page.svelte
@@ -0,0 +1,16 @@
+<section>
+    <div class="container">
+        <input />
+        <input />
+        <button>Login</button>
+        <button>Home</button>
+    </div>
+</section>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+    }
+</style>
---
+++ src/routes/Register/+page.svelte
@@ -0,0 +1,18 @@
+<section>
+    <div class="container">
+        <input class="input"/>
+        <input class="input"/>
+        <input class="input"/>
+        <input class="input"/>
+        <button>Register</button>
+        <button>Home</button>
+    </div>
+</section>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+    }
+</style>
