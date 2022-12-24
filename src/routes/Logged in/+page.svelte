9bd42b2e6b43a46f8b06a93f28dce19166c5567e
alaidine <aa.alimlinde@gmail.com>
2022-12-25 00:03:13 +0400

    page that is showned when the login was successful

---
+++ src/routes/Logged in/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Button from '../../lib/Button.svelte'
+</script>
+
+<div>
+    <h1>Succesfully Logged In!</h1>
+    <Button value="Home" />
+    <Button value="Sign out" />
+</div>
