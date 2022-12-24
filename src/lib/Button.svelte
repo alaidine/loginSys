5380e65fb4d87fa270762c8d8f6724cc6fdc5de6
alaidine <aa.alimlinde@gmail.com>
2022-12-25 00:04:00 +0400

    Button and Input component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,44 @@
+<script>
+    /**
+	 * @type {any}
+	 */
+     export let value
+
+     import * as db from '../database/main'
+
+    const routes = [
+        "Login page",
+        "Registration page"
+    ]
+
+    function handleClick() {
+        null
+    }
+</script>
+
+<style>
+    .button {
+        height: 40px;
+        width: 220px;
+        border-radius: 25px;
+        background-color: rgb(194, 194, 194);
+        transition: background-color 0.5s;
+    }
+
+    .button:hover {
+        background-color: rgb(41, 40, 40);
+    }
+
+</style>
+
+{#if routes.includes(value)}
+    <a href="/{value}">
+        <button class="button" >{value}</button>
+    </a>
+{:else if value == "Home"}
+    <a href="/">
+        <button class="button" >{value}</button>
+    </a>
+{:else}
+    <button class="button" on:click={handleClick}>{value}</button>
+{/if}
---
+++ src/lib/Input.svelte
@@ -0,0 +1,10 @@
+<input class="input" />
+
+<style>
+    .input {
+        border: 2px solid grey;
+        height: 40px;
+        width: 210px;
+        border-radius: 25px;
+    }
+</style>
