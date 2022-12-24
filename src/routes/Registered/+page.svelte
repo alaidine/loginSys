08455e676b5adaf192964c7069cc1e8c923f827d
alaidine <aa.alimlinde@gmail.com>
2022-12-25 00:02:42 +0400

    Page that is showned when the registration was successfull

---
+++ src/routes/Registered/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Button from '../../lib/Button.svelte'
+</script>
+
+<div>
+    <h1>Registered successfully!</h1>
+    <Button value="Home"/>
+    <Button value="Login" />
+</div>
